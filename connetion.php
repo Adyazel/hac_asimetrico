6ce23b9ee3a2bd92c2172793e7c14f0e68add372
Adyazel <108038440+Adyazel@users.noreply.github.com>
2023-11-23 20:35:18 -0600

    Update connetion.php

--- connetion.php
+++ connetion.php
@@ -1,7 +1,7 @@
 <?php
  $dbhost = "localhost";
  $dbuser = "root";
- $dbpass = "";
+ $dbpass = "12345";
  $db = "hacienda";
  $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
 
